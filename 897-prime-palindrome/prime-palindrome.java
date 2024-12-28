45c7c7a4107a26f616122ca4c31d77005f68d65c
Rehan Naveid <119671994+RehanNaveid@users.noreply.github.com>
2024-12-28 22:15:16 +0530

    Time: 298 ms (30.75%) | Memory: 44.8 MB (10.15%) - LeetSync

--- 897-prime-palindrome/prime-palindrome.java
+++ 897-prime-palindrome/prime-palindrome.java
@@ -34,7 +34,9 @@ class Solution {
         if(n<=1){
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(n); i += 1) { 
+        // i<= sqrt(n)
+        // i^2,=n
+        for (int i = 2; i*i<= n; i += 1) { 
             if (n % i == 0) {
                 return false;
             }
