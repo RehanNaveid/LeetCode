5632c419cc3feef8897a38859a18f05ed0e3a633
Rehan Naveid <119671994+RehanNaveid@users.noreply.github.com>
2025-01-28 14:26:56 +0530

    Time: 2 ms (61.73%) | Memory: 40.1 MB (92.09%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -1,34 +1,34 @@
 class Solution {
-    int c=0;
+    int c=0;//count
     public int totalNQueens(int n) {
         int arr[][]=new int[n][n];
-        queenplace(arr,n,0);
+        queenplace(arr,n,0);//start from 0th row
         return c;
     }
     public void queenplace(int arr[][],int n,int row){
         if(row==n){
-            c++;
+            c++;//count increases for every distinct solution
         }
         else{
             for(int col=0;col<n;col++){
-                if(isSafe(arr,row,col)==1){
+                if(isSafe(arr,row,col)==1){//if a position is safe
                     arr[row][col]=1;
-                    queenplace(arr,n,row+1);
-                    arr[row][col]=0;
+                    queenplace(arr,n,row+1);//move to next row
+                    arr[row][col]=0;//backtracking
                 }
             }
         }
     }
     public int isSafe(int arr[][],int row,int col){
-        for(int i=row;i>=0;i--){
+        for(int i=row;i>=0;i--){//top
             if(arr[i][col]==1)
             return 0;
         }
-        for(int i=row,j=col;i>=0&&j>=0;i--,j--){
+        for(int i=row,j=col;i>=0&&j>=0;i--,j--){//diagonal
             if(arr[i][j]==1)
             return 0;
         }
-        for(int i=row,j=col;i>=0&&j<arr[0].length;i--,j++){
+        for(int i=row,j=col;i>=0&&j<arr[0].length;i--,j++){//minor diagnoal
             if(arr[i][j]==1)
             return 0;
         }
