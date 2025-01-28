e8b6c1a7151af3d198d57c0a08b894c78dbd451f
Rehan Naveid <119671994+RehanNaveid@users.noreply.github.com>
2025-01-28 14:23:23 +0530

    Time: 2 ms (61.73%) | Memory: 40.4 MB (64.21%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -2,11 +2,6 @@ class Solution {
     int c=0;
     public int totalNQueens(int n) {
         int arr[][]=new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                arr[i][j]=0;
-            }
-        }
         queenplace(arr,n,0);
         return c;
     }
