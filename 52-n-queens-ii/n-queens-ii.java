75f8caf30719de14749a15e2af430e882b9e08ce
Rehan Naveid <119671994+RehanNaveid@users.noreply.github.com>
2025-01-28 14:22:50 +0530

    Time: 2 ms (61.73%) | Memory: 40.8 MB (35.40%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    int c=0;
+    public int totalNQueens(int n) {
+        int arr[][]=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=0;
+            }
+        }
+        queenplace(arr,n,0);
+        return c;
+    }
+    public void queenplace(int arr[][],int n,int row){
+        if(row==n){
+            c++;
+        }
+        else{
+            for(int col=0;col<n;col++){
+                if(isSafe(arr,row,col)==1){
+                    arr[row][col]=1;
+                    queenplace(arr,n,row+1);
+                    arr[row][col]=0;
+                }
+            }
+        }
+    }
+    public int isSafe(int arr[][],int row,int col){
+        for(int i=row;i>=0;i--){
+            if(arr[i][col]==1)
+            return 0;
+        }
+        for(int i=row,j=col;i>=0&&j>=0;i--,j--){
+            if(arr[i][j]==1)
+            return 0;
+        }
+        for(int i=row,j=col;i>=0&&j<arr[0].length;i--,j++){
+            if(arr[i][j]==1)
+            return 0;
+        }
+        return 1;
+    }
+}
