515a4631432b6df8cab42ac58becc75499e07a6b
Rehan Naveid <119671994+RehanNaveid@users.noreply.github.com>
2025-02-04 22:39:58 +0530

    Time: 383 ms (9.32%) | Memory: 41.3 MB (44.77%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean solveSudoku(char[][] board) {
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char ch='1';ch<='9';ch++){
+                        if(isValid(board,ch,i,j)){
+                            board[i][j]=ch;
+                            if(solveSudoku(board)){
+                                return true;
+                            }
+                            else{
+                                board[i][j]='.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static boolean isValid(char[][] board,char c, int row, int col) {
+    for (int i = 0; i < 9; i++) {
+      if (board[i][col] == c)
+        return false;
+
+      if (board[row][i] == c)
+        return false;
+
+      if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+        return false;
+    }
+    return true;
+  }
+}
