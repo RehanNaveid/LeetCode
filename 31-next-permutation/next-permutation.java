fb1c18a627e2d77656a1d2ce34a9b8a30d8231e2
Rehan Naveid <119671994+RehanNaveid@users.noreply.github.com>
2025-02-18 18:24:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (55.90%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,41 +1,43 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int index=-1;
-        for(int i=nums.length-1;i>0;i--){
-            if(nums[i]>nums[i-1]){
-                index=i-1;
+        int index = -1;
+        int n = nums.length;
+        
+        // Step 1: Find the first decreasing element from the right
+        for (int i = n - 1; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                index = i - 1;
                 break;
             }
         }
 
-        if (index==-1){
-            int start=0,end=nums.length-1;
-            while(start<end){
-                int temp=nums[start];
-                nums[start]=nums[end];
-                nums[end]=temp;
-                start++;
-                end--;
+        if (index == -1) {
+            // Step 2: If no such element exists, reverse the array
+            reverse(nums, 0, n - 1);
+        } else {
+            // Step 3: Find the next larger element and swap
+            for (int i = n - 1; i > index; i--) {
+                if (nums[i] > nums[index]) {
+                    swap(nums, i, index);
+                    break;
+                }
             }
+            // Step 4: Reverse the right portion
+            reverse(nums, index + 1, n - 1);
         }
-        else{
-        for(int i=nums.length-1;i>0;i--){
-            if(nums[i]>nums[index]){
-                int temp=nums[i];
-                nums[i]=nums[index];
-                nums[index]=temp;
-                break;
-            }
-        }
-        int start=index+1,end=nums.length-1;
-            while(start<end){
-                int temp=nums[start];
-                nums[start]=nums[end];
-                nums[end]=temp;
-                start++;
-                end--;
-            }
-        }
-        
     }
-}
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}  
